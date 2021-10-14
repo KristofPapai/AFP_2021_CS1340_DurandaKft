6c30348b855e7db02370cc274b8236e931b22ed7
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-10-14 14:08:06 +0200

    bufix
    bugfix

--- Duranda Weblap/js/teglalapscript.js
+++ Duranda Weblap/js/teglalapscript.js
@@ -1,15 +1,17 @@
 function calculate(oldalA,oldalB){
-        var oldalA(parseInt(document.getElementById(oldalA).value);
-        var oldalB(parseInt(document.getElementById(oldalB).value);
+        var oldalA = parseInt(document.getElementById(oldalA).value);
+        var oldalB = parseInt(document.getElementById(oldalB).value);
+
 
         if (oldalA > 0 && oldalB > 0)
         {
             var K = 2 * (oldalA + oldalB);
-            var T = (oldalA * oldalB).ToFixed(2);
-            window.alert("A téglalap területe: ${T} cm2 ||| A téglalap kerülete: ${K} cm");
+            var T = (oldalA * oldalB);
+            T = T.toFixed(2);
+            window.alert(`A téglalap területe: ${T} cm2 ||| A téglalap kerülete: ${K} cm`)
         }
         else
-		{
-			window.alert("Ezekkel az adatokkal nem tudja az oldal kiszámolni az eredményt.")
-		}
+    		{
+    			window.alert("Ezekkel az adatokkal nem tudja az oldal kiszámolni az eredményt.")
+    		}
     }
